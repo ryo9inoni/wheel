31e0a1e30ef0a67e8f583b3b6ec4de9ca3957045
井上良太 <ryo9inoni@gmail.com>
2023-01-12 14:13:06 +0900

    first commit

---
+++ main.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.contents(@wheel="Wheel")
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      sliderLastTime: 0,
+      sliderLockTime: 1300,
+    }
+  },
+  methods:{
+    Wheel(e){
+      // イベントが発火してからthis.sliderLockTime秒以上経つまで発火させない
+      const timeNow = new Date().getTime()
+      if(timeNow - this.sliderLastTime < this.sliderLockTime) return
+
+      this.sliderLastTime = timeNow
+    },
+  }
+}
+</script>
